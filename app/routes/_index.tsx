e541ebdd7dc1c2a635174b08a143615771ec05be
zermoser <kittipoj.na@gmail.com>
2024-08-07 10:20:07 +0700

    First commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,17 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "สวัสดีจ้า 🏳️‍🌈" },
+    { name: "ทดสอบระบบจร้า", content: "สวีดัส สวัสดี อิอิ" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="font-sans p-4 bg-slate-800 h-screen text-white">
+      <h1 className="text-3xl">Welcome to Remix</h1>
+      <h1 className="text-3xl">Hello World</h1>
+    </div>
+  );
+}
