17422395dd5ace8959ec32c81a376f8aa5b62253
zermoser <kittipoj.na@gmail.com>
2024-10-28 05:49:17 +0700

    test

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -2,6 +2,10 @@ export default function Index() {
   return (
     <div className="font-sans p-4 bg-slate-800 h-screen text-white">
       <h1 className="text-3xl">Welcome to Remix อิอิ</h1>
+      <h1 className="text-3xl">Welcome to Remix อิอิ</h1>
+      <h1 className="text-3xl">Welcome to Remix อิอิ</h1>
+      <h1 className="text-3xl">Welcome to Remix อิอิ</h1>
+      <h1 className="text-3xl">Welcome to Remix อิอิ</h1>
     </div>
   );
 }
